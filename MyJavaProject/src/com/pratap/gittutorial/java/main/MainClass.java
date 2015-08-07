b67677a4594410673fea6a26e87aa71dbfa77087
OrgPratap <org.pratap@gmail.com>
2015-08-08 03:27:17 +0530

    master commit 1

---
+++ MyJavaProject/src/com/pratap/gittutorial/java/main/MainClass.java
@@ -0,0 +1,14 @@
+package com.pratap.gittutorial.java.main;
+
+public class MainClass 
+{
+
+	/**
+	 * @author author1@orgpratap.com
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("***WELCOME***");
+	}
+
+}
