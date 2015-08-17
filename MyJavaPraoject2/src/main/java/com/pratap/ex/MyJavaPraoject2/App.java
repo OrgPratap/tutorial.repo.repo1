605704368ef5e37948128e1a141610848bdc4012
OrgPratap <org.pratap@gmail.com>
2015-08-17 12:18:25 +0530

    added MyJavaProject2

---
+++ MyJavaPraoject2/src/main/java/com/pratap/ex/MyJavaPraoject2/App.java
@@ -0,0 +1,13 @@
+package com.pratap.ex.MyJavaPraoject2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
